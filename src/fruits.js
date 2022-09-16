2dc03fa2e0eae21d3c3019b1a538e4e6daf0ce2a
Mike <wattonlaw@gmail.com>
2022-09-15 22:09:41 -0500

    initial commit

---
+++ src/fruits.js
@@ -0,0 +1,5 @@
+let fruits = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🍎",
+"🍏", "🍐", "🍒", "🍓", "🥝", "🍅", "🥑"];
+
+export default fruits;
+// export default is used when it's just one thing being exported
